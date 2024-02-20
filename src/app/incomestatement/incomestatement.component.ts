f811d1bffb36b44b439cce0da993e66664bc7ece
miketilhn <michael.cgart@gmail.com>
2024-02-19 23:00:30 -0500

    21924

--- src/app/incomestatement/incomestatement.component.ts
+++ src/app/incomestatement/incomestatement.component.ts
@@ -2,18 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { IncomefetchService } from '../shared/incomefetch.service';
 import { HttpClient } from '@angular/common/http';
 import { Incomestatement } from '../models/instmodel';
+import { map, reduce } from 'rxjs';
+import { Observable } from 'rxjs';
 
-
-interface Food {
-  value: string;
-  viewValue: string;
-}
 @Component({
   selector: 'app-incomestatement',
   templateUrl: './incomestatement.component.html',
   styleUrls: ['./incomestatement.component.scss']
 })
+
 export class IncomestatementComponent implements OnInit  {
+
   statements: Incomestatement[] =[];
   firstQuarter: Incomestatement[]=[];
   secondQuarter:Incomestatement[]=[];
@@ -25,41 +24,57 @@ export class IncomestatementComponent implements OnInit  {
   totalThird: any [] =[];
   totalFourth: any [] =[];
   totalAll: any []=[];
-
+  allAccTotalsta: number []=[];
+  totalSum: any;
+  inputValue: any;
+  
   constructor(
     private http: HttpClient,
     private incomefetch: IncomefetchService
-   
     ){
 }
 
-
+  // ngOnInit() {
+  //   this.incomefetch.getStatement().pipe(
+  //     map(data=> data.map(item => item.total)),
+  //     reduce((acc, val)=> acc + val, 0 )
+  //   ).subscribe(sum = this)
+  // }
   ngOnInit() {
     this.incomefetch.getStatement().subscribe((data)=>{
-      this.firstQuarter = data.filter(d => d.dateRangeStart === "Jan, 23");
-      this.secondQuarter = data.filter(d => d.dateRangeStart === "Apr, 23");
-      this.thirdQuarter = data.filter(d => d.dateRangeStart === "Jul, 23");
-      this.fourthQuarter = data.filter(d => d.dateRangeStart === "Oct, 23");
-      this.totalFirst = this.firstQuarter.map((item)=>{
-        return item.total
-      })
-      this.totalSecond = this.secondQuarter.map((item)=>{
-        return item.total
-      })
-      this.totalThird = this.thirdQuarter.map((item)=>{
-        return item.total
-      })
-      this.totalFourth = this.fourthQuarter.map((item)=>{
-        return item.total
-      })
-
-      // this.totalAll = this.totalFirst + this.totalSecond + this.totalThird + this.totalFourth
+      // console.log(data.forEach(item=> item.dateRangeStart));
+      // this.firstQuarter = data.filter(d => d.dateRangeStart === new Date(2023, 0))
+      // this.secondQuarter = data.filter(d => d.dateRangeStart === new Date(2023, 3));
+      // this.thirdQuarter = data.filter(d => d.dateRangeStart === new Date(2023, 6));
+      // this.fourthQuarter = data.filter(d => d.dateRangeStart === new Date(2023, 9));
+      this.statements = data;
+       // this.totalAll = this.totalFirst + this.totalSecond + this.totalThird + this.totalFourth
     });
-
- 
-   
-    
   }
 
+  getquarter() {
+    const enteredQuarter = this.inputValue.split('-')
+    const year = Number.parseInt(enteredQuarter[0]);
+    const month = Number.parseInt(enteredQuarter[1]);
+    const newDate = new Date(year, month-1)
+    const formattedDate = `${newDate.getFullYear()}-${(newDate.getMonth() + 1).toString().padStart(2, '0')}`;
 
+      
+    this.statements.map((item)=>{
+      
+      const date =item.dateRangeStart.toString() ;
+      const dateSplit = date.split('-');
+      const yr = Number.parseInt(dateSplit[0]);
+      const mnt = Number.parseInt(dateSplit[1]);
+      const newDte = new Date(yr, mnt-1);
+      const frmtedDate = `${newDte.getFullYear()}-${(newDte.getMonth() + 1).toString().padStart(2, '0')}`;
+        console.log("result: " +frmtedDate, "request: "+ formattedDate)
+      
+      // if(frmtedDate === formattedDate){
+      //   console.log("true");
+      // }else{
+      //   console.log("false")
+      // }
+    })
+  }
 }
--- src/app/models/instmodel.ts
+++ src/app/models/instmodel.ts
@@ -1,24 +1,15 @@
 export class Incomestatement {
-  constructor(
-    public id: {
-      type: string;
-      required: true;
-    },
-    public dateRangeStart:string,
-    public dateRangeEnd: string,
-    public total: {
-      type: number;
-      required: true;
-    },
 
+  constructor(
+    public id: {type: string},
+    public dateRangeStart:Date,
+    public dateRangeEnd: Date,
+    public total: {type: number},
     public account: { type: string },
     public firstMonth: { type: number },
     public secondMonth: { type: number },
     public thirdMonth: { type: number },
 
-    public reportDate: {
-      type: Date;
-      required: true;
-    }
+
   ) {}
 }
